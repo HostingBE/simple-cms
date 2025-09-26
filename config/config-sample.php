5f3535028615fb4e9dd961353ff523f9599724db
Simple CMS by HostingBE <info@hostingbe.com>
2025-09-26 14:49:16 +0200

    changed version in config-sample.php

--- config/config-sample.php
+++ config/config-sample.php
@@ -1,35 +1,35 @@
 <?php
 
 return [
-   'settings' => [ 
+   'settings' => [
                    'displayErrorDetails' => true,
                    'logErrorDetails' => true,
                    'debug' => true,
-                   'version' => '3.0.0',
+                   'version' => '3.1.0',
                    'logger' => [
                    'name' => '[logname]',
                    'path' => __DIR__ . '/../logs/'.date('d-m-Y').'-app.log',
                    'level' => 'DEBUG',
                         ],
                    'db'  => [
-     
+
      'host' => '[host]',
      'username' => '[username]',
      'database' => '[database]',
      'password' => '[password]',
 
-     ], 
+     ],
      'translations' => [
             'path' => __DIR__."/../lang",
             'fallback' => 'en',
             'languages' => array(
                            array('url' => 'simple-cms-nl.hostingbe.lan', 'language' => 'nl', 'spoken' => 'nederlands'),
-                           array('url' => 'simple-cms.hostingbe.lan', 'language' => 'en', 'spoken' => 'english'), 
-                           array('url' => 'simple-cms-de.hostingbe.lan', 'language' => 'de','spoken' => 'deutsch')), 
+                           array('url' => 'simple-cms.hostingbe.lan', 'language' => 'en', 'spoken' => 'english'),
+                           array('url' => 'simple-cms-de.hostingbe.lan', 'language' => 'de','spoken' => 'deutsch')),
             'enabled' => true,
      ],
         ],
-             
+
 ]
 
 ?>
